4c9a818978460667d3d69a735a27022d34bc81ef
Yuan-Sama <nbnguyen9557@gmail.com>
2024-11-25 13:41:55 +0700

    landing page

---
+++ src/routes/(landing)/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+	import Logo from '$assets/logo.png';
+	import Marketing from '$assets/marketing.jpg';
+	import { APP_NAME } from '$lib';
+
+	/**
+	 * @type {{
+	 * 	data: import('./$types').PageData
+	 * }}
+	 */
+	let { data } = $props();
+</script>
+
+<svelte:head>
+	<title>{APP_NAME}</title>
+	<link rel="shortcut icon" href={Logo} type="image/png" />
+</svelte:head>
+
+<section class="bg-[#f2ece5]">
+	<div
+		class="mx-auto grid h-screen max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0"
+	>
+		<div class="mr-auto place-self-center lg:col-span-6">
+			<h1
+				class="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl"
+			>
+				Software tool to manage your finance
+			</h1>
+			<p class="mb-6 max-w-2xl font-light text-gray-500 md:text-lg lg:mb-8 lg:text-xl">
+				Manage your finance never been easy. We here to help you manage your finance anywhere,
+				anytime and any place
+			</p>
+			<a
+				href="/sign-in"
+				class="mr-3 inline-flex items-center justify-center rounded-lg bg-primary-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300"
+			>
+				Sign in
+				<svg
+					class="-mr-1 ml-2 h-5 w-5"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+					><path
+						fill-rule="evenodd"
+						d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+						clip-rule="evenodd"
+					></path></svg
+				>
+			</a>
+			<a
+				href="/sign-up"
+				class="inline-flex items-center justify-center rounded-lg border border-gray-300 px-5 py-3 text-center text-base font-medium text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100"
+			>
+				Create your account
+			</a>
+		</div>
+		<div class="hidden lg:col-span-6 lg:mt-0 lg:flex">
+			<img src={Marketing} alt="mockup" />
+		</div>
+	</div>
+</section>
--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,16 +1,23 @@
 <script>
+	import Logo from '$assets/logo.png';
 	import { page } from '$app/stores';
+	import { APP_NAME } from '$lib';
 </script>
+
+<svelte:head>
+	<title>{APP_NAME} - {$page.status}</title>
+	<link rel="shortcut icon" href={Logo} type="image/png" />
+</svelte:head>
 
 <section class="flex min-h-screen items-center bg-white dark:bg-gray-900">
 	<main class="mx-auto max-w-screen-xl pb-8 lg:pb-16">
 		<div class="mx-auto max-w-screen-sm text-center">
 			<h1
-				class="text-primary-600 dark:text-primary-500 mb-4 text-7xl font-extrabold tracking-tight lg:text-9xl"
+				class="mb-4 text-7xl font-extrabold tracking-tight text-primary-600 dark:text-primary-500 lg:text-9xl"
 			>
 				{$page.status}
 			</h1>
-			<p class="mb-4 text-3xl font-bold tracking-tight text-gray-900 md:text-4xl dark:text-white">
+			<p class="mb-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-white md:text-4xl">
 				{#if $page.status === 404}
 					Something's missing.
 				{:else if $page.status === 500}
@@ -26,7 +33,7 @@
 			</p>
 			<a
 				href="/"
-				class="bg-primary-600 hover:bg-primary-800 focus:ring-primary-300 dark:focus:ring-primary-900 my-4 inline-flex rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+				class="my-4 inline-flex rounded-lg bg-primary-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
 				>Back to Homepage</a
 			>
 		</div>
