e7d4854f7b68f9994e5601316733409760934762
Yuan-Sama <nbnguyen9557@gmail.com>
2024-12-06 15:41:33 +0700

    add off-canvas sidebar component

--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -4,6 +4,7 @@
 	import type { LayoutData } from './$types';
 	import { AppName } from '$lib';
 	import { page } from '$app/stores';
+	import OffCanvasSidebar from '$components/OffCanvasSidebar.svelte';
 
 	let { data, children }: { data: LayoutData; children: Snippet } = $props();
 
@@ -29,6 +30,8 @@
 			label: 'Settings'
 		}
 	];
+
+	const userRoutes: { href: string; label: string }[] = [];
 
 	let mobileNavHidden = $state(true);
 	let userMenuHidden = $state(true);
@@ -94,37 +97,14 @@
 							/>
 						</svg>
 					</button>
-					<div
-						class="fixed left-0 top-0 z-40 h-screen w-64 overflow-y-auto bg-white p-4 transition-transform duration-500 dark:bg-gray-800 {!mobileNavHidden
-							? 'transform-none'
-							: '-translate-x-full'}"
-						tabindex="-1"
-					>
-						<h5
-							id="drawer-navigation-label"
-							class="text-base font-semibold uppercase text-gray-500 dark:text-gray-400"
-						>
-							{AppName}
-						</h5>
-						<button
-							type="button"
-							class="absolute end-2.5 top-2.5 inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
-							onclick={() => (mobileNavHidden = true)}
-						>
-							<svg
-								aria-hidden="true"
-								class="h-5 w-5"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-								xmlns="http://www.w3.org/2000/svg"
-								><path
-									fill-rule="evenodd"
-									d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-									clip-rule="evenodd"
-								></path></svg
-							>
-							<span class="sr-only">Close menu</span>
-						</button>
+
+					<OffCanvasSidebar bind:hide={mobileNavHidden}>
+						{#snippet header()}
+							<h5 class="text-base font-semibold uppercase text-gray-500 dark:text-gray-400">
+								{AppName}
+							</h5>
+						{/snippet}
+
 						<div class="overflow-y-auto py-4">
 							<ul class="space-y-2 font-medium">
 								{#each routes as route}
@@ -142,10 +122,7 @@
 								{/each}
 							</ul>
 						</div>
-					</div>
-					{#if !mobileNavHidden}
-						<div class="fixed inset-0 z-30 bg-gray-900/50 dark:bg-gray-900/80"></div>
-					{/if}
+					</OffCanvasSidebar>
 				</nav>
 			</div>
 
@@ -167,10 +144,22 @@
 						<div>Demo Example</div>
 						<div class="truncate font-medium">{data.user.username}</div>
 					</div>
-					<!-- <ul
-						class="py-2 text-sm text-gray-700 dark:text-gray-200"
-						aria-labelledby="avatarButton"
-					></ul> -->
+					{#if userRoutes.length}
+						<ul
+							class="py-2 text-sm text-gray-700 dark:text-gray-200"
+							aria-labelledby="avatarButton"
+						>
+							{#each userRoutes as route}
+								<li>
+									<a
+										href={route.href}
+										class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+										>{route.label}</a
+									>
+								</li>
+							{/each}
+						</ul>
+					{/if}
 					<div class="py-1">
 						<form action="/sign-out" method="post">
 							<button
