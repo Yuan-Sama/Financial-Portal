22d077063f83e12999fb0e586a3190ab23d44f0a
Yuan-Sama <nbnguyen9557@gmail.com>
2024-12-19 00:01:30 +0700

    add template pages

---
+++ src/routes/(dashboard)/transactions/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { AppName } from '$lib/index.svelte';
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
+</script>
+
+<svelte:head>
+	<title>{AppName} - Transactions</title>
+</svelte:head>
